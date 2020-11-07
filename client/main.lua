6ba6ce28ca4fdff676dcda85bbf76cb176c6b076
andommar <v1perlsrp@gmail.com>
2020-11-07 20:56:23 +0100

    Añadida variacion peds

--- client/main.lua
+++ client/main.lua
@@ -18,14 +18,15 @@ end,false)
 RegisterNetEvent('vorp_skinPed:setSkinPed')
 AddEventHandler('vorp_skinPed:setSkinPed', function(clientId,model)
     local hashModel = GetHashKey(model)
+
     if not IsModelValid(hashModel) then return end
 	PerformRequest(hashModel)
 	
 	if HasModelLoaded(hashModel) then
 		-- SetPlayerModel(player, model, false)
-        Citizen.InvokeNative(0xED40380076A31506, player, hashModel, true)
+        Citizen.InvokeNative(0xED40380076A31506, PlayerId(), hashModel, true)
         SetModelAsNoLongerNeeded(hashModel)
-	end
+    end
 
 end)
 
@@ -39,3 +40,15 @@ function PerformRequest(hash)
         if times >= 100 then break end
     end
 end
+
+RegisterCommand('pedtest',function(source, args)
+    local player = PlayerPedId()
+    local variation = tonumber(args[1])
+    print(player)
+    Citizen.InvokeNative(0x77FF8D35EEC6BBC4,PlayerPedId(),variation,0)
+
+    --Para cambiar el walking style hay que desactivar el walking anterior
+    --De todas formas no arregla el bug de los skins npcs que no pueden recargar
+    --Citizen.InvokeNative(0xCB9401F918CB0F75, PlayerPedId(), "default",0,-1)
+    --Citizen.InvokeNative(0x89F5E7ADECCCB49C, PlayerPedId(), "john_marston")
+end,false)
